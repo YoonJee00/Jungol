0b7956a4d861805ac7b5c64435e1d57f1e2462de
Administrator <Administrator@User-2022GHJANZ>
2022-11-02 17:44:45 +0900

    JungolwithT

--- Jungol/src/q512/Main.java
+++ Jungol/src/q512/Main.java
@@ -2,9 +2,10 @@ package q512;
 
 public class Main {
 	public static void main(String[] args) {
-		double a = 49;
-		double b = 0.2683;
+		int weight = 49;
+		double ratio = 0.2683;
 
-		System.out.println();
+//		System.out.println(weight + " * " + ratio + " = " + (weight * ratio));
+		System.out.printf("%d * %.6f = %.6f\n", weight, ratio, weight*ratio);
 	}
 }
