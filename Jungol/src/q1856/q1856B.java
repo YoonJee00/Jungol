1de4b2335082d74d8e9409e77478983cbbab27dc
Administrator <Administrator@User-2022GHJANZ>
2022-11-18 18:05:38 +0900

    Jungol

---
+++ Jungol/src/q1856/q1856B.java
@@ -0,0 +1,35 @@
+package q1856;
+
+import java.util.Scanner;
+
+public class q1856B {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		sc.close();
+
+		int[][] ar = new int[n][m];
+		int num = 1;
+		for (int i = 0; i < n; i++) {
+			if (i % 2 == 0) {
+				for (int j = 0; j < m; j++) {
+					ar[i][j] = num++;
+				}
+			} else {
+				for (int j = m - 1; j >= 0; j--) {
+					ar[i][j] = num++;
+				}
+
+			}
+		}
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				System.out.print(ar[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+}
